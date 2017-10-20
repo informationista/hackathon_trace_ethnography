9b218c96ce2c827f42a7e649b6a37b723868e348
informationista <lisa.federer@nih.gov>
2017-10-20 15:44:34 -0400

    chart color change

--- combined_charts.R
+++ combined_charts.R
@@ -20,7 +20,7 @@ get_hack_data <- function(slack_name, slack_token, gdoc_id, gh_repo) {
 make_trace_plots <- function(df, start_date, end_date) {
   lims <- as.POSIXct(strptime(c(paste(Sys.Date()-1, "20:30"),(paste(Sys.Date(), "24:59"))), format = "%Y-%m-%d %H:%M"))
   
-  p <- ggplot(df, aes(day, time, color = user))  + scale_x_datetime(breaks = date_breaks("2 days"), labels=date_format("%b %d")) + scale_y_datetime(limits = lims, breaks = date_breaks("3 hours"), labels = date_format("%H:%M")) + scale_color_brewer(palette = "Paired") + theme(axis.text.x=element_text(angle=90, margin = margin(t = 5, r = 0, b = 0, l = 0)), panel.background = element_rect(fill= "white")) + annotate("rect", ymin = as.POSIXct(paste(Sys.Date(), "04:00")), ymax = as.POSIXct(paste(Sys.Date(), "14:00")), xmin = as.POSIXct(paste(start_date, "09:00:00")), xmax = as.POSIXct(paste(end_date, "04:00:00")), fill = "grey", alpha = 0.6) +  geom_point(size = 1) + facet_wrap(~data_source, nrow = 3) 
+  p <- ggplot(df, aes(day, time, color = user))  + scale_x_datetime(breaks = date_breaks("2 days"), labels=date_format("%b %d")) + scale_y_datetime(limits = lims, breaks = date_breaks("3 hours"), labels = date_format("%H:%M")) + scale_color_brewer(palette = "Paired") + theme(axis.text.x=element_text(angle=90, margin = margin(t = 5, r = 0, b = 0, l = 0))) + annotate("rect", ymin = as.POSIXct(paste(Sys.Date(), "04:00")), ymax = as.POSIXct(paste(Sys.Date(), "14:00")), xmin = as.POSIXct(paste(start_date, "09:00:00")), xmax = as.POSIXct(paste(end_date, "04:00:00")), fill = "grey", alpha = 0.6) +  geom_point(size = 1) + facet_wrap(~data_source, nrow = 3) 
   return(p)
 
 }
